47b0a62ade10e81f86c6ed54a6b32359c3289f26
路由 <m19373557587@163.com>
2026-05-17 08:36:33 +0800

    生成首页路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/views/Home/DashboardView.vue'),
+      meta: { title: '后台面板' },
+    }
+  ],
 })
 
 export default router
