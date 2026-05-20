6d1bf8fef0b4a49b550daaa7869747ea401f903a
路由 <m19373557587@163.com>
2026-05-20 16:27:19 +0800

    完成按钮多个问题

--- src/components/Common/ShopList.vue
+++ src/components/Common/ShopList.vue
@@ -9,7 +9,6 @@ defineProps({
 
 <template>
     <el-table :data="goodsList" stripe v-loading="false" @selection-change="$emit('selection-change', $event)">
-        <el-table-column type="selection" width="55" />
         <slot name="columns"></slot>
     </el-table>
 
